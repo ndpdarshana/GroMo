d7f0db83b755f749e487ca3c57166910e6538bb8
Prabhath Darshana <prabhath@huubap.com>
2020-10-07 13:53:44 +0530

    Search screen added

---
+++ lib/screens/growth_monitoring_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GrowthMonitoringScreen extends StatelessWidget {
+  static const routeName = '/growthMonitor';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gwoth Monitoring'),
+      ),
+    );
+  }
+}

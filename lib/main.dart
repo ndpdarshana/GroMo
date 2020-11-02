0945aa2c3916236b2acb6f010a71e2d5c6a83f91
Prabhath Darshana <prabhath@huubap.com>
2020-11-02 22:39:36 +0530

    Initialized project with some content and implemenation of localization added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:growMo/app_localizations.dart';
 import 'package:growMo/screens/search_screen.dart';
 import './screens/edit_record_screen.dart';
 import './screens/growth_monitoring_screen.dart';
@@ -15,6 +17,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GrowMo',
+      //List all the supported localizations
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('id', 'ID'),
+      ],
+      // These delagates make sure that the localization data for the proper language is loaded
+      localizationsDelegates: [
+        // A class which loads the translations from json files
+        AppLocalizations.delegate,
+        // Built-in localization of basic text for Material widgets
+        GlobalMaterialLocalizations.delegate,
+        // Built-in localization for text direction LTR/RTL
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      // Returns a locale which will be used by the app
+      localeResolutionCallback: (locale, supportedLocales) {
+        // Check if the current device locale is supported
+        for (var supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale.languageCode &&
+              supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+        // if the locale of the device is not supported, use the first one
+        // from the list (English, in this case)
+        return supportedLocales.first;
+      },
       theme: ThemeData(primaryColor: Colors.blue),
       home: AuthScreen(),
       routes: {

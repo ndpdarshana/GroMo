7a3f462f5778e13b7a81fe892e514081d7731652
Prabhath Darshana <prabhath@huubap.com>
2020-11-05 15:00:41 +0530

    background of login inputs

--- lib/login/login_form_widget.dart
+++ lib/login/login_form_widget.dart
@@ -4,6 +4,7 @@ import 'package:growMo/app_localizations.dart';
 import 'package:growMo/login/bloc/login_bloc.dart';
 import 'package:growMo/login/login_password_field_widget.dart';
 import 'package:growMo/login/login_username_field_widget.dart';
+import 'package:growMo/screens/home_screen.dart';
 
 class LoginForm extends StatelessWidget {
   final FocusNode passwordFocusNode = FocusNode();
@@ -22,13 +23,49 @@ class LoginForm extends StatelessWidget {
       },
       child: Form(
         child: Container(
-          padding: const EdgeInsets.all(10),
-          height: MediaQuery.of(context).size.height * 0.3,
+          height: MediaQuery.of(context).size.height * 0.4,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.transparent),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.75),
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               LoginUsernameField(passwordFieldFocus: passwordFocusNode),
               LoginPasswordField(passwordFieldFocus: passwordFocusNode),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                  top: 30.0,
+                ),
+                alignment: Alignment.center,
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    Expanded(
+                      child: FlatButton(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 20,
+                        ),
+                        color: Colors.green[200],
+                        child: Text(
+                          AppLocalizations.of(context).translate('btn_login'),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushReplacementNamed(HomeScreen.routeName);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),

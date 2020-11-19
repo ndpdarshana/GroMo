88a17f641c56efc86a264d35a81bb8b12255d1f1
Prabhath Darshana <prabhath@huubap.com>
2020-11-19 13:11:29 +0530

    app decupling

---
+++ lib/app/bloc/app_state.dart
@@ -0,0 +1,12 @@
+part of 'app_bloc.dart';
+
+class AppState extends Equatable {
+  const AppState();
+
+  @override
+  List<Object> get props => [];
+
+  AppState copyWith() {
+    return AppState();
+  }
+}

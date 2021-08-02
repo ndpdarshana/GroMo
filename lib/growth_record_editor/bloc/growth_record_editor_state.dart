98387ea903230bfbbfeefdf7d72a302d8e5c3ad9
Prabhath Darshana <prabhth@huubap.com>
2021-08-02 20:52:30 +0530

    feature(growth_record_editor):weight scaler prototype

--- lib/growth_record_editor/bloc/growth_record_editor_state.dart
+++ lib/growth_record_editor/bloc/growth_record_editor_state.dart
@@ -2,11 +2,26 @@ part of 'growth_record_editor_bloc.dart';
 
 class GrowthRecordEditorState extends Equatable {
   final FormzStatus status;
+  final WeightInput weightInput;
+  final AppError? error;
 
   const GrowthRecordEditorState({
     this.status = FormzStatus.pure,
+    this.weightInput = const WeightInput.pure(),
+    this.error,
   });
 
   @override
-  List<Object> get props => [status];
+  List<Object?> get props => [status, weightInput, error];
+
+  GrowthRecordEditorState copyWith({
+    FormzStatus? status,
+    WeightInput? weightInput,
+    AppError? error,
+  }) =>
+      GrowthRecordEditorState(
+        status: status ?? this.status,
+        weightInput: weightInput ?? this.weightInput,
+        error: error ?? this.error,
+      );
 }
--- lib/growth_record_editor/weight_scaler_field_widget.dart
+++ lib/growth_record_editor/weight_scaler_field_widget.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:growmo/growth_record_editor/bloc/growth_record_editor_bloc.dart';
 import '/app_localizations.dart';
 
-class WeightScalerFieldWidget extends StatelessWidget {
-  const WeightScalerFieldWidget();
+enum WeightInputError { invalid }
+const int MAX_WEIGHT = 3000;
 
+class WeightInput extends FormzInput<int, WeightInputError> {
+  const WeightInput.pure() : super.pure(0);
+  const WeightInput.dirty(int value) : super.dirty(value);
+
+  @override
+  WeightInputError? validator(int value) {
+    return value > 0 ? null : WeightInputError.invalid;
+  }
+}
+
+class WeightScalerFieldWidget extends StatefulWidget {
+  @override
+  _WeightScalerFieldWidgetState createState() => _WeightScalerFieldWidgetState();
+}
+
+class _WeightScalerFieldWidgetState extends State<WeightScalerFieldWidget> {
+  final TextEditingController _textFieldController = TextEditingController();
+  double _slider = 0.0;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,11 +38,36 @@ class WeightScalerFieldWidget extends StatelessWidget {
             AppLocalizations.of(context)!.translate('field_label_weight')!,
             style: Theme.of(context).textTheme.subtitle1,
           ),
-          Row(
-            children: [
-              Expanded(child: Slider(value: 0, onChanged: (value) {})),
-              Container(width: 80, child: TextFormField()),
-            ],
+          BlocListener<GrowthRecordEditorBloc, GrowthRecordEditorState>(
+            listenWhen: (previous, current) => previous.weightInput != current.weightInput,
+            listener: (context, state) {
+              setState(() {
+                // _textFieldController.text = state.weightInput.value.toString();
+                _slider = state.weightInput.value.toDouble();
+              });
+            },
+            child: Row(
+              children: [
+                Expanded(
+                  child: Slider(
+                    value: _slider,
+                    min: 0,
+                    max: MAX_WEIGHT.toDouble(),
+                    onChanged: (value) => context.read<GrowthRecordEditorBloc>().add(WeightSliderChanged(value)),
+                  ),
+                ),
+                Container(
+                  width: 80,
+                  child: TextFormField(
+                    controller: _textFieldController,
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    onChanged: (value) => context.read<GrowthRecordEditorBloc>().add(WeightFieldChanged(value)),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
